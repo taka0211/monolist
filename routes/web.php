93d6f2c03cd9cb918728de4fdfe5312f368ee8e2
jiro.kirameki <ec2-user@ip-172-31-8-110.us-east-2.compute.internal>
2018-06-18 05:31:10 +0000

    haveranking

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create']]);
 });
 
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+Route::get('ranking/have', 'RankingController@have')->name('ranking.have');
+
+
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
     Route::post('want', 'ItemUserController@want')->name('item_user.want');
@@ -34,11 +38,4 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['show']]);
 });
 
-Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
 
-Route::group(['middleware' => ['auth']], function () {
-    Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
-    Route::post('want', 'ItemUserController@want')->name('item_user.want');
-    Route::delete('want', 'ItemUserController@dont_want')->name('item_user.dont_want');
-    Route::resource('users', 'UsersController', ['only' => ['show']]);
-});
